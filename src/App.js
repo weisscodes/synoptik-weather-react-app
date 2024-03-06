9ec2cfbba9f4c49692c2568b612399028843c580
Ellathiel <98175801+weisscodes@users.noreply.github.com>
2024-03-06 21:56:42 +0100

    Set up a new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Margo Wise and{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/weisscodes"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          open-sourced on Github
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
