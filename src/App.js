b3a702cd369b97e1f63fde0e11edfe52096c6f13
Ellathiel <98175801+weisscodes@users.noreply.github.com>
2024-03-06 22:11:36 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Margo Wise and{" "}
-        <a
-          href="https://github.com/weisscodes"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Margo Wise and{" "}
+          <a
+            href="https://github.com/weisscodes/synoptik-weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
