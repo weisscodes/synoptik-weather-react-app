dbd413fefde7513746b42fce840b576cdfb24288
Ellathiel <98175801+weisscodes@users.noreply.github.com>
2024-03-07 18:33:22 +0100

    Added Search Engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 import logo from "./logo.svg";
@@ -7,6 +7,7 @@ import logo from "./logo.svg";
 // 667d9f573c8af4c33457be5d561a9148
 
 function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity); // sent from the App component
   const [weatherData, setWeatherData] = useState({ ready: false }); // Instead of using a separate state
   function handleResponse(response) {
     setWeatherData({
@@ -22,16 +23,33 @@ function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "667d9f573c8af4c33457be5d561a9148";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
+`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather container">
         <header className="row py-4">
           <div className="col">
-            <form className="search-form">
+            <form className="search-form" onSubmit={handleSubmit}>
               <input
                 type="search"
                 placeholder="Type a city name..."
                 className="search-input"
+                onChange={handleCityChange}
               />
               <input type="submit" value="Search" className="search-button" />
             </form>
@@ -40,66 +58,12 @@ function Weather(props) {
             <img src={logo} alt="Synoptik Logo"></img>
           </div>
         </header>
-        <section className="hero-section p-0">
-          <div className="hero_background-wrapper">
-            <img
-              src="assets\weather-app-bg.svg"
-              alt="synoptik-background"
-              className="hero_background-image"
-            ></img>
-          </div>
-          <div className="hero-content">
-            <div className="temperature-wrapper mb-1">
-              <div className="weather-icon">
-                <img
-                  src={weatherData.iconUrl}
-                  alt={weatherData.description}
-                ></img>
-              </div>
-              <div className="temperature-current">
-                {Math.round(weatherData.temperature)}{" "}
-                <span className="temperature-unit">°C</span>
-              </div>
-            </div>
-            <h1 className="city-name">
-              {weatherData.city}, {weatherData.country}
-            </h1>
-            <div className="weather-info-wrapper mt-3">
-              <div className="weather-info d-flex">
-                <div className="me-3">
-                  <img
-                    src="/assets/humidity-icon.svg"
-                    alt="humidity-icon"
-                    className="weather-info_icon"
-                  ></img>
-                  <span>{weatherData.humidity}</span>%
-                </div>
-                <div>
-                  <img
-                    src="/assets/ico-wind.svg"
-                    alt="wind-icon"
-                    className="weather-info_icon"
-                  ></img>
-                  <span>{weatherData.wind}</span>km/h
-                </div>
-              </div>
-              <div className="weather-description text-capitalize">
-                {weatherData.description}
-              </div>
-              <div className="date-wrapper">
-                <FormattedDate date={weatherData.date} />
-                {/*Pass the date from an object to the component*/}
-              </div>
-            </div>
-          </div>
-        </section>
+        <WeatherInfo data={weatherData} />
+        {/*Sending data from the Object to the component*/}
       </div>
     );
   } else {
-    const apiKey = "667d9f573c8af4c33457be5d561a9148";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric
-`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return "Loading"; // change to Loader Spinner
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,63 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+// Recieving data from the Weather object
+
+function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <section className="hero-section p-0">
+        <div className="hero_background-wrapper">
+          <img
+            src="assets\weather-app-bg.svg"
+            alt="synoptik-background"
+            className="hero_background-image"
+          ></img>
+        </div>
+        <div className="hero-content">
+          <div className="temperature-wrapper mb-1">
+            <div className="weather-icon">
+              <img src={props.data.iconUrl} alt={props.data.description}></img>
+            </div>
+            <div className="temperature-current">
+              {Math.round(props.data.temperature)}{" "}
+              <span className="temperature-unit">°C</span>
+            </div>
+          </div>
+          <h1 className="city-name">
+            {props.data.city}, {props.data.country}
+          </h1>
+          <div className="weather-info-wrapper mt-3">
+            <div className="weather-info d-flex">
+              <div className="me-3">
+                <img
+                  src="/assets/humidity-icon.svg"
+                  alt="humidity-icon"
+                  className="weather-info_icon"
+                ></img>
+                <span>{props.data.humidity}</span>%
+              </div>
+              <div>
+                <img
+                  src="/assets/ico-wind.svg"
+                  alt="wind-icon"
+                  className="weather-info_icon"
+                ></img>
+                <span>{props.data.wind}</span>km/h
+              </div>
+            </div>
+            <div className="weather-description text-capitalize">
+              {props.data.description}
+            </div>
+            <div className="date-wrapper">
+              <FormattedDate date={props.data.date} />
+              {/*Pass the date from an object to the component*/}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default WeatherInfo;
